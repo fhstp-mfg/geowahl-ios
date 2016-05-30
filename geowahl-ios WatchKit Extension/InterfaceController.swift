5ee939c803fec05fe900c490a82dfd4309827493
Patrick Eberhardt <patrick.eberhardt@me.com>
2016-05-30 13:58:18 +0200

    Initial Commit

---
+++ geowahl-ios WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  geowahl-ios WatchKit Extension
+//
+//  Created by Patrick Eberhardt on 30.05.16.
+//  Copyright © 2016 Patrick Eberhardt. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
